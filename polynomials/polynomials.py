f22db9ed624de107bcdd840d2215bf93fdb3a547
kelvin386 <kelvin.wu.qk@gmail.com>
2022-02-04 11:24:50 +0000

    Addition and equality

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 class Polynomial:
     def __init__(self, coefs):
 
@@ -7,7 +9,7 @@ class Polynomial:
 
         return len(self.coefficients) - 1
 
-    def __str__(self):   #It defines what is returned for str(p) and print(p)
+    def __str__(self):   #The special method defines what is returned for str(p) and print(p)
 
         coefs = self.coefficients
         terms = []
@@ -23,3 +25,29 @@ class Polynomial:
         return " + ".join(reversed(terms)) or "0"
         #if the former is an empty string, then the empty string is false, hence return "0"
         #if the former is not empty, return it
+
+    def __eq__(self, other):
+        
+        return isinstance(other, Polynomial) \
+            and self.coefficients == other.coefficients #check whether the second argument is a Poly, then compare
+
+    def __add__(self, other):
+        
+        #The addition of tuples is concatenation! Can't just add them!
+        if isinstance(other, Polynomial):
+            
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a+b for a,b in zip(self.coefficients, other.coefficients)) #zip will stop when one of them runs out
+            coefs += self.coefficients[common:] + other.coefficients[common:] #The lower degree one will have nothing after "common"
+
+            return Polynomial(coefs)
+        
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0]+other,)+self.coefficients[1:])
+
+        else:
+            return NotImplemented
+
+
+    def __radd__(self,other):
+        return self + other #Reverse add is the same as normal add
